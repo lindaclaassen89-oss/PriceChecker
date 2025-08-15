0ac92005516c4ca838d363e4d7f3c14593ec6e2b
lindaclaassen89-oss <lindaclaassen89@gmail.com>
2025-08-15 13:19:36 +0200

    print chromedriver path

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ import platform
 
 SHEETY_ENDPOINT = "https://api.sheety.co/d6b82e9c05bc37bf12c02605d8f5dd44/groceries/groceries"
 
+for root, dirs, files in os.walk("/"):
+for name in files:
+if "chromedriver" in name:
+print(os.path.join(root, name))
 
 sheety = requests.get(SHEETY_ENDPOINT, verify=False)
 sheety_list = sheety.json()["groceries"]
@@ -97,4 +101,5 @@ for store in ["sixty", "ww"]:
                 }
             }
         response = requests.put(f"{SHEETY_ENDPOINT}/{item["id"]}", json=update_json, verify=False)
+
 
