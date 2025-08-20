4acd2c704c59a72dd9be59c7f7507b2221deacf9
lindaclaassen89-oss <lindaclaassen89@gmail.com>
2025-08-20 10:36:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -150,7 +150,10 @@ for store in ["sixty", "ww"]:
     OTP_inputs[3].send_keys(Keys.TAB + Keys.ENTER)
 
     # DOB_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".input.input_input__qgb6Z")))
-    DOB_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="tw-modal"]/div/div/div/div[1]/div/form/div[1]/div/input')))
+    DOB_input = WebDriverWait(driver, 20).until(
+            lambda d:   EC.presence_of_element_located((By.XPATH, '//*[@id="tw-modal"]/div/div/div/div[1]/div/form/div[1]/div/input'))(d) and
+                        EC.visibility_of_element_located((By.XPATH,'//*[@id="tw-modal"]/div/div/div/div[1]/div/form/div[1]/div/input'))(d)
+        )
     DOB_input.send_keys(dob)
     DOB_input.send_keys(Keys.TAB + Keys.ENTER)
 
