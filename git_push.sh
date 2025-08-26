7317bc463ecf34ddd5cfded57ed12f8ce10e13e2
Linda Claassen <claassenl@out.co.za>
2025-08-26 10:14:12 +0200

    Committing shell script

--- git_push.sh
+++ git_push.sh
@@ -4,12 +4,8 @@
 echo "Enter your commit message:"
 read commit_message
 
-# Prompt for the branch name
-echo "Enter the branch name to push to:"
-read branch_name
-
 # Git commands
 git status
 git add .
 git commit -m "$commit_message"
-git push origin "$branch_name"
+git push origin main
